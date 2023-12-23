119062e3df0f945175ded10fe7f5e287ceae322d
Heitor Campanhol <heitor.camp@live.com.pt>
2023-12-23 19:42:48 -0300

    algumas mudanças para abir a camera

--- src/pages/TelaPrincipal/telaPrincipal.js
+++ src/pages/TelaPrincipal/telaPrincipal.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, Image, TouchableOpacity, Modal} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+import Scanner from '../LeitorQr/leitorQrcode';
 
 export default function TelasPrincipal() {
   const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = React.useState(false);
   return(
       <View style={styles.container}>
+        <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType='fade'>
+          <View style={styles.modal}>
+            <Scanner/>
+            <TouchableOpacity style={styles.button} title="Cancelar" onPress={() => setModalVisible(false)}>
+              <Text>Cancelar</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+
+
         <View>
           <Text style={styles.title} id='textCenter'>Não foi feita leitura de QRcode hoje</Text>
         </View>
 
     <TouchableOpacity style={styles.button}
-    onPress={() => navigation.navigate('LeitorQr')}>
+    onPress={() => setModalVisible(true)}>
       <Text>Scanear QRcode</Text>
       </TouchableOpacity>
       </View>
@@ -35,10 +49,18 @@ const styles = StyleSheet.create({
         backgroundColor: 'blue',
         borderRadius: 50,
         paddingVertical: 8,
-        width: '70%',
+        width: '80%',
+        
         alignSelf: 'center',
-        bottom: '15%',
+        bottom: '5%',
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    modal:{
+      flex:1,
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      backgroundColor: 'lightgrey'
+
     }
 })
